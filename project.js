d9af402939fb99c74565865eb801b7c8cfc68798
mariorogachev <mario.rogachev14@gmail.com>
2022-09-23 21:18:30 +0300

    Problem with running the program

---
+++ project.js
@@ -0,0 +1,14 @@
+
+const readline = require ('readline').createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+let guess= Math.floor(Math.random()*100);
+
+let recursiveAsyncReadLine=function(){
+    readline.question('Guess the number (0-100):', number =>{
+
+        
+    })
+}
+recursiveAsyncReadLine();
